0fccda78674ae4ea47e7d8b233d195e1ef468aa4
rpshende <rpshende@example.com>
2021-04-17 20:32:40 -0400

    format more code

--- Bird.js
+++ Bird.js
@@ -1,8 +1,8 @@
 class Bird extends BaseClass {
-  constructor(x,y){
-    super(x,y,50,50);
+  constructor(x, y) {
+    super(x, y, 50, 50);
     this.image = loadImage("sprites/bird.png");
-  }
+  };
 
   display() {
     this.body.position.x = mouseX;
--- Pig.js
+++ Pig.js
@@ -1,7 +1,6 @@
 class Pig extends BaseClass {
-  constructor(x, y){
-    super(x,y,50,50);
+  constructor(x, y) {
+    super(x, y, 50, 50);
     this.image = loadImage("sprites/enemy.png");
-  }
-
+  };
 };
